652dfcb30c49fd4239ae2d967bf1b4f91e5f7684
rajeshraji17 <rajeshraji633@gmail.com>
2022-12-22 13:09:33 +0530

    uu

--- R/src/com/java/Ko.java
+++ R/src/com/java/Ko.java
@@ -1,5 +1,10 @@
 package com.java;
 
 public class Ko {
+	public static void main(String[] args) {
+		System.out.println("tt");
+		System.out.println("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");
+		
+	}
 
 }
